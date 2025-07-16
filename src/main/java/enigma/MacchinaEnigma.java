e18b4e6fc89077b6b1dc764dbf454216d21fc7ed
michele minno <michele.minno@protonmail.com>
2025-07-16 10:17:12 +0200

    Code added

---
+++ src/main/java/enigma/MacchinaEnigma.java
@@ -0,0 +1,8 @@
+
+package enigma;
+
+public class MacchinaEnigma {
+    public String cifra(String input) {
+        return input.toUpperCase(); // Simulazione banale
+    }
+}
---
+++ src/test/java/enigma/MacchinaEnigmaTest.java
@@ -0,0 +1,15 @@
+
+package enigma;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MacchinaEnigmaTest {
+
+    MacchinaEnigma macchina = new MacchinaEnigma();
+
+    @Test
+    void testCifra() {
+        assertEquals("HELLO", macchina.cifra("hello"));
+    }
+}
