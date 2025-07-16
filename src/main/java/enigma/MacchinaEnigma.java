a99692776d2c94d54fda11d6a7ccf90885495fb8
michele minno <michele.minno@protonmail.com>
2025-07-16 11:06:11 +0200

    add decifra method to MacchinaEnigma class for basic decryption

--- src/main/java/enigma/MacchinaEnigma.java
+++ src/main/java/enigma/MacchinaEnigma.java
@@ -6,4 +6,8 @@ public class MacchinaEnigma {
     public String cifra(String input) {
         return input.toUpperCase(); // Simulazione banale
     }
+
+    public String decifra(String input) {
+        return input.toLowerCase(); // Simulazione banale
+    }
 }
